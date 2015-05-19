85ddac24483482cb38a34c61be116452c3364ab6
LippiOuYang <l.p.ouyang@qq.com>
2015-05-19 21:21:27 +0800

    Update CacheFactory.java

--- src/main/java/com/lippi/cache/CacheFactory.java
+++ src/main/java/com/lippi/cache/CacheFactory.java
@@ -1,22 +1,4 @@
-/**
- * $RCSfile$
- * $Revision: 3144 $
- * $Date: 2005-12-01 14:20:11 -0300 (Thu, 01 Dec 2005) $
- * <p>
- * Copyright (C) 2004-2008 Jive Software. All rights reserved.
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package com.lippi.cache;
 
 import org.slf4j.Logger;
